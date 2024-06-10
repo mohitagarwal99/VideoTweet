15f091dd6f0a84037c534046f02f970d4a6ca050
mohitagarwal99 <mohitaga98@gmail.com>
2024-06-10 15:07:54 +0530

    Utility modules setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@
 
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import app from './app.js'
 
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Listening on port ${process.env.PORT}`);
+    })
+    app.on("error", (err) => {
+        console.log("Error: ", err)
+        throw err;
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection error ", error)
+    process.exit(1)
+})
 
 
 
