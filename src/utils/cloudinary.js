1833d7ceac96d2addc1ab6f1c6c443aaf25eec3c
mohitagarwal99 <mohitaga98@gmail.com>
2024-06-11 22:08:00 +0530

    configure setup | before controllers/logic write

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
+
+//Cloudinary Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SCRET // Click 'View Credentials' below to copy your API secret
+});
+
+
+const uploadOnCloud = async (localFilePath) => {
+    try {
+
+        if(!localFilePath){
+            return null;
+        }
+
+        //Upload the file path
+        const uploadResult = await cloudinary.uploader.upload(localFilePath, 
+            {
+                resource_type: "auto",
+            },
+        );
+        console.log("File uploaded successfully ", uploadResult);
+        console.log("File url: ", uploadResult.url);
+        return uploadResult;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //remove temporarirly save locally file synchronously
+        return null;
+    }
+}
+
+
+export default uploadOnCloud;
